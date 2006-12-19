9f90fba28bb7fed9a173c844691f0b88565cdd59
Dan Pascu <dan@ag-projects.com>
2006-12-19 01:04:59 +0000

    Updated debian copyright, README and package info

--- setup.py
+++ setup.py
@@ -3,13 +3,22 @@
 import sys
 from distutils.core import setup, Extension
 
-
-sources = ['cjson.c']
-
-setup(name         = "cjson",
+setup(name         = "python-cjson",
       version      = "1.0.0",
-      description  = "Fast JSON encoder/decoder for Python",
       author       = "Dan Pascu",
       author_email = "dan@ag-projects.com",
-      license      = "GPL",
-      ext_modules  = [Extension(name='cjson', sources=sources)])
+      url          = "http://ag-projects.com/",
+      download_url = "http://cheeseshop.python.org/pypi/python-cjson/1.0.0",
+      description  = "Fast JSON encoder/decoder for Python",
+      long_description = open('README', 'r').read(),
+      license      = "LGPL",
+      platforms    = ["Platform Independent"],
+      classifiers  = [
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules"
+      ],
+      ext_modules  = [Extension(name='cjson', sources=['cjson.c'])])
