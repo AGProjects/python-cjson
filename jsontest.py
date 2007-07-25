eac61049de7b7d04dd75881ab04ec2fead1898e0
Dan Pascu <dan@ag-projects.com>
2007-07-25 13:56:10 +0000

    Added LGPL header from the original jsontest.py

--- jsontest.py
+++ jsontest.py
@@ -2,7 +2,23 @@
 
 ## this test suite is an almost verbatim copy of the jsontest.py test suite
 ## found in json-py available from http://sourceforge.net/projects/json-py/
+##
 ## Copyright (C) 2005  Patrick D. Logan
+## Contact mailto:patrickdlogan@stardecisions.com
+##
+## This library is free software; you can redistribute it and/or
+## modify it under the terms of the GNU Lesser General Public
+## License as published by the Free Software Foundation; either
+## version 2.1 of the License, or (at your option) any later version.
+##
+## This library is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+## Lesser General Public License for more details.
+##
+## You should have received a copy of the GNU Lesser General Public
+## License along with this library; if not, write to the Free Software
+## Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 
 import unittest
 
