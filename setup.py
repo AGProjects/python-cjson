6708fbb1ac13f4e27a817f8045f4bd4855fbf6b7
Dan Pascu <dan@ag-projects.com>
2019-09-20 12:42:18 +0000

    Refactored setup.py to be PEP-8 compliant

--- setup.py
+++ setup.py
@@ -2,28 +2,33 @@
 
 from distutils.core import setup, Extension
 
-__version__ = "1.2.1"
+__version__ = '1.2.1'
 
 macros = [('MODULE_VERSION', __version__)]
 
-setup(name         = "python-cjson",
-      version      = __version__,
-      author       = "Dan Pascu",
-      author_email = "dan@ag-projects.com",
-      url          = "https://github.com/AGProjects/python-cjson",
-      description  = "Fast JSON encoder/decoder for Python",
-      long_description = open('README').read(),
-      license      = "LGPL",
-      platforms    = ["Platform Independent"],
-      classifiers  = [
-        "Development Status :: 5 - Production/Stable",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python",
-        "Topic :: Software Development :: Libraries :: Python Modules"
-      ],
-      ext_modules  = [
+setup(
+    name='python-cjson',
+    version=__version__,
+
+    description='Fast JSON encoder/decoder for Python',
+    long_description=open('README').read(),
+    url='https://github.com/AGProjects/python-cjson',
+    license='LGPL',
+
+    author='Dan Pascu',
+    author_email='dan@ag-projects.com',
+
+    platforms=['Platform Independent'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+
+    ext_modules=[
         Extension(name='cjson', sources=['cjson.c'], define_macros=macros)
-      ]
+    ]
 )
